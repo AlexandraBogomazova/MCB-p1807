8756210d74a7a72798d502014a0cadf207505b6d
Bogomazova Alexandra <alexandra.bogomazova888@gmail.com>
2020-10-02 18:18:00 +0300

    Добавьте файлы проекта.

---
+++ laba3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер месяца");
+            int month;
+            month = Convert.ToInt32(Console.ReadLine());
+            switch (month)
+            {
+                case 
+            }
+        }
+    }
+}
